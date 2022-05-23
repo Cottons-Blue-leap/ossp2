16db39bccec0a604e43466501b02e8ee124ea09a
Cottons-Blue-leap <wlsxorb1@naver.com>
2022-05-24 05:07:17 +0900

    ADD University my_info.py

--- my_info.py
+++ my_info.py
@@ -2,9 +2,11 @@ def my_info():
 
     name = input("Write your name: ")
     id = input("Write your Student ID: ")
+    university = input("Write your University: ")
 
     print("\n<PRINT>")
     print(f"NAME: {name}")
     print(f"ID: {id}")
+    print(f"UNIVERSITY: {university}")
 
 my_info()
