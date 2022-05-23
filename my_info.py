b33131e1fd24b01ee01ad833a2fcfc6935684ed5
Cottons-Blue-leap <wlsxorb1@naver.com>
2022-05-24 05:04:02 +0900

    CREATE my_info.py

---
+++ my_info.py
@@ -0,0 +1,10 @@
+def my_info():
+
+    name = input("Write your name: ")
+    id = input("Write your Student ID: ")
+
+    print("\n<PRINT>")
+    print(f"NAME: {name}")
+    print(f"ID: {id}")
+
+my_info()
